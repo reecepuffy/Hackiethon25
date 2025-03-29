99301e62a441a32f01d924cdf22f9267d8e5a332
reecepuffy <jget0001@student.monash.edu>
2025-03-29 13:23:05 +1100

    feat: created a timer

--- showcase-site/src/App.jsx
+++ showcase-site/src/App.jsx
@@ -1,37 +1,37 @@
-import './App.css';
-import Navbar from './navbar';
-import Dropdown from './Dropdown';
-import DropZone from './Dropzone';
-import MyWidget from './submission-widgets/MyWidget';
+import './App.css'
+import Navbar from './navbar'
+import Dropdown from './Dropdown'
+import DropZone from './Dropzone'
 
 function App() {
 
   // Paste your desired website here, in full https form without the "/" at the end
   // NOTE: not all websites work. Websites protected under CSP cannot be embedded
   let MyUrl = "";
-  if (MyUrl) {
+  if (MyUrl){
     return (
       <div className="App">
         <Navbar />
-        <DropZone url={MyUrl} />
+        <DropZone url={MyUrl}/>
       </div>
-    );
+    )
   }
+
 
   return (
     <div className="App" style={{ backgroundImage: 'url(/stars.png)', height: '100vh' }}>
-      <Navbar />
-      <div className="relative text-center mt-10 pb-16">
-        <div className="flex justify-center items-center mt-8">
-          <div className="max-w-xs w-full">
-            <MyWidget />
-          </div>
+        <Navbar />
+        <div className="relative text-center mt-10">
+          <h1 className="outline-text">
+            SHOWCASE
+          </h1>
+          <h2 className="main-title">
+            Hackiethon Widget Showcase
+          </h2>
         </div>
-      </div>
-      
-      <DropZone />
+        <DropZone/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
